b50930e96c4379960d56944f820dcde7174d32ef
Seong Ug Kim <sukim2017@gmail.com>
2022-12-21 11:06:21 +0900

    JWT

---
+++ token/payload.go
@@ -0,0 +1,51 @@
+package token
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var (
+	ErrExpiredToken = errors.New("token has expired")
+	ErrInvalidToken = errors.New("token is invalid")
+)
+
+// Payload contains the payload data of the token
+type Payload struct {
+	ID uuid.UUID `json:"id"`
+	// identify the token owner
+	Username string `json:"username"`
+	// when the token is created
+	IssuedAt time.Time `json:"issued_at"`
+	// it is crucial to make sure that each access token only has a short valid duration
+	// store the time at which the token will be expired
+	ExpiredAt time.Time `json:"expired_at"`
+}
+
+// NewPayload creates a new token payload with a specific username and duration
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		ID:        tokenID,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+
+	return payload, nil
+}
+
+// Valid checks if the token payload is valid or not
+func (p *Payload) Valid() error {
+	if time.Now().After(p.ExpiredAt) {
+		return ErrExpiredToken
+	}
+
+	return nil
+}
